1ed223dfd4f15357a3bc6e657501669155c19a0d
Shan Huang <mike9447@hotmail.com>
2019-06-10 18:48:18 -0700

    list created

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,9 +7,20 @@ import csv
 #set path for csv file
 csvpath = os.path.join("03-Python_Homework_PyBank_Resources_budget_data.csv")
 
-# open the csv file
-with open(csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+# create lists to store data
+month = []
+amount = []
+
+#open the csv file
+with open(csvpath, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter",")
+    # add months and amount to list
+    for row in csvreader:
+        month.append(row[0])
+    for row in csvreader:
+        amount.append(row[1])
+    # determine total months
+    
 
 
 
