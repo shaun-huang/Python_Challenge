6b76949cc9358b9ffed211c89607c589bfccc816
Shan Huang <mike9447@hotmail.com>
2019-06-10 21:25:59 -0700

    pypoll updated

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,5 +6,24 @@ import csv
 csvpath = os.path.join("03-Python_Homework_PyPoll_Resources_election_data.csv")
 
 #create list to store data
+voter = []
+county = []
+candidate = []
 
 #open the csv file
+with open(csvpath, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    # start from second row
+    next(csvreader)
+    # create a loop
+    for row in csvreader:
+    # add votes to list
+        voter.append(row[0])
+
+
+#Result
+    print('Election Result')
+    print('-------------------------------------')
+    print('Total Votes: '+ str(len(voter)))
+    print('-------------------------------------')
+
