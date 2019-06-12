55db2b58dadd65169b6f1692d0bdb0d38d553c9c
Shan Huang <mike9447@hotmail.com>
2019-06-11 21:33:39 -0700

    completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -30,8 +30,19 @@ with open(csvpath, newline = "") as csvfile:
     amount_change = [x2 - x1 for (x1,x2) in zip(amount1,amount2)]
     #find average amount change
     average_change = round(sum(amount_change)/len(amount_change),2)
-    #find greatest increase and decrease/Maybe create a dictionary here
-        
+    #find greatest increase and decrease
+    def max_increase(list):
+        for x in list:
+            if x == max(list):
+                maxindex = list.index(x)
+        return maxindex
+    max_index=max_increase(amount_change) +1
+    def max_decrease(list):
+        for x in list:
+            if x == min(list):
+                minindex = list.index(x)
+        return minindex
+    min_index=max_decrease(amount_change)+1
 
     #Generate Report
     print("Financial Analysis")
@@ -42,4 +53,7 @@ with open(csvpath, newline = "") as csvfile:
     print("Total Amount: "+ str(sum(amount)))
     # determine average change
     print("Average Change: $"+ str(average_change))
-
+    # determind greatest increase in profit
+    print('Greatest Increase in Profits: '+ month[int(max_index)]+" ("+str(max(amount_change))+")")
+    # determine greatest loss in profit
+    print('Greatest Decrease in Profits: '+ month[int(min_index)]+" ("+str(min(amount_change))+")")
